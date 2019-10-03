0b77ec05bd2f230ea5e3fbb6623d3a8409d605b8
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-03 12:43:17 -0300

    frontend finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import api from './services/api';
+
 import './App.css';
 
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
+  
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
       <div className="content">
-        <p>
-          Ofereça <strong>spots </strong>para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-        <form>
-          <label htmlFor="email">E-mail *</label>
-          <input 
-          id="email" 
-          type="email"
-          placeholder="seu melhor e-mail"/>
-          <button className="btn"type="submit">Entrar</button>
-        </form>
+        <Routes />
+
       </div>
     </div> 
   );
