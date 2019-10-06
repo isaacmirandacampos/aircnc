68b0c29fc3009752179bf8bfece6d9f4036c8f05
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-06 20:51:29 -0300

    conexão webpack

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,28 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+
+
 mongoose.connect('mongodb+srv://isaac:imcaasi@aircnc-vcwem.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) =>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
